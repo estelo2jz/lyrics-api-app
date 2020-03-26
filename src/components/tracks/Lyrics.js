6e2662d7e8d1598ee62b28e9c4cf7a3390647b42
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-03-26 18:13:42 -0500

    added moment

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
+import Moment from 'react-moment';
 
 export default class Lyrics extends Component {
   state = {
@@ -68,7 +69,7 @@ export default class Lyrics extends Component {
                 <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'} 
               </li>
               <li className="list-group-item">
-                <strong>Release Date</strong>: {track.first_release_date} 
+                <strong>Release Date</strong>: <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
               </li>
             </ul>
           </div>
